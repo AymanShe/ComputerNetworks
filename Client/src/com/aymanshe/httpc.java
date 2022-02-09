a0574a6917ed4b1e69369d5b48ebdec6f5c66728
Ayman <aymalsh@gmail.com>
2022-02-08 21:03:32 -0500

    Add validations

--- Client/src/com/aymanshe/httpc.java
+++ Client/src/com/aymanshe/httpc.java
@@ -7,11 +7,27 @@ public class httpc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //region Validations
         //check if the minimum number of argument is provided
-        if (args.length < 2){
+        if (args.length == 0){
+            System.out.println("The command cannot be empty");
             System.out.println("Please enter the command following this format: httpc (get|post) [-v] (-h \"k:v\")* [-d inline-data] [-f file] URL");
             return;
         }
+
+        if(!args[0].equals("httpc")){
+            System.out.println("The command should start with httpc");
+            System.out.println("Please enter the command following this format: httpc (get|post) [-v] (-h \"k:v\")* [-d inline-data] [-f file] URL");
+            return;
+        }
+
+        if (args.length < 3){
+            System.out.println("The command has too few arguments");
+            System.out.println("Please enter the command following this format: httpc (get|post) [-v] (-h \"k:v\")* [-d inline-data] [-f file] URL");
+            return;
+        }
+
+        //endregion
 
         try {
             //parse into request
@@ -26,7 +42,6 @@ public class httpc {
             //TODO print a better message
             e.printStackTrace();
         }
-
     }
 
 }
