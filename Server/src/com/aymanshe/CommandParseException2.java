60f0f4b729f43522b1e694ee0c074780685ec725
Ayman <aymalsh@gmail.com>
2022-03-14 22:10:24 -0400

    Get arguments for file server

---
+++ Server/src/com/aymanshe/CommandParseException2.java
@@ -0,0 +1,7 @@
+package com.aymanshe;
+
+public class CommandParseException2 extends Exception{
+    public CommandParseException2(String message) {
+        super(message);
+    }
+}
--- Server/src/com/aymanshe/httpfs.java
+++ Server/src/com/aymanshe/httpfs.java
@@ -1,6 +1,9 @@
 package com.aymanshe;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class httpfs {
 
@@ -10,13 +13,134 @@ public class httpfs {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //TODO get arguments from args
+        int port = 8080;
+        String path = ".";
+        boolean verbose = false;
 
-        HttpServer httpServer = new HttpServer(8007,".", true);
+        // region validations
+        if (args.length == 0) {
+            System.out.println("The command cannot be empty");
+            System.out.println("Please enter the command following this format: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]");
+            return;
+        }
+
+        if (!args[0].equals("httpfs")) {
+            System.out.println("The command should start with httpfs");
+            System.out.println("Please enter the command following this format: httpfs [-v] [-p PORT] [-d PATH-TO-DIR]");
+            return;
+        }
+
+        try {
+            validateArguments(args);
+        } catch (CommandParseException2 e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        try {
+            for (int i = 1; i < args.length - 1; i++) {
+                switch (args[i]) {
+                    case "-v" -> verbose = true;
+                    case "-d" -> {
+                        // todo Assuming all double quotation has to be escaped otherwise it will be parsed by java
+                        StringBuilder pathString = new StringBuilder(args[++i]);
+                        int index = pathString.indexOf("'");
+                        if (index != 0) {
+                            throw new CommandParseException2("the path has to start and end with single quotation.");
+                        }
+                        pathString = new StringBuilder(pathString.substring(1));
+                        index = pathString.lastIndexOf("'");
+                        if (index == pathString.length() - 1) {// there is a quotation at the end of the argument, hence it is the end of path
+                            pathString = new StringBuilder(pathString.substring(0, pathString.length() - 1));
+                        } else {// the body has more parts that are seperated by space
+                            boolean reachedEnd = false;
+                            while (!reachedEnd) {
+                                if (i + 1 >= args.length) {
+                                    throw new CommandParseException2("path string finished but the closing quotation for inline body was not found");
+                                }
+                                String next = args[++i];
+                                // check if it is the end path by checking the ' and making sure it is not in the middle of the string
+                                index = -1;
+                                index = next.lastIndexOf("'");//
+                                if (index != next.length() - 1) {
+                                    // it is not the end of the body
+                                    pathString.append(next);
+                                    pathString.append(" ");
+                                } else {
+                                    // it might be the end of the body or a quotation in the middle
+                                    if (index == next.length() - 1) {
+                                        // it is the end of the path
+                                        next = next.substring(0, next.length() - 1);
+                                        pathString.append(next);
+                                        reachedEnd = true;
+                                    } else {
+                                        // it is a colon in the middle as part of the body
+                                        pathString.append(next);
+                                        pathString.append(" ");
+                                    }
+                                }
+                            }
+                        }
+                        path = pathString.toString();
+                    }
+                    case "-p" -> port = Integer.parseInt(args[++i]);
+                }
+            }
+
+            validatePath(path);
+
+        } catch (CommandParseException2 e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        // endregion
+
+        if (verbose){
+            File file = new File(path);
+            System.out.println("Working directory set to " + file.getAbsolutePath());
+        }
+        HttpServer httpServer = new HttpServer(port, path, verbose);
         try {
             httpServer.run();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void validatePath(String path) {
+        //TODO
+    }
+
+    private static void validateArguments(String[] args) throws CommandParseException2 {
+        Map<String, Boolean> map = new HashMap<>();
+        for (int i = 1; i < args.length - 1; i++) {
+            switch (args[i]) {
+                case "-v" -> {
+                    if (map.get("-v") != null) {
+                        throw new CommandParseException2("Duplicate argument -v");
+                    }
+                    map.put("-v", true);
+                }
+                case "-d" -> {
+                    if (map.get("-d") != null) {
+                        throw new CommandParseException2("Duplicate argument -d");
+                    }
+                    map.put("-d", true);
+                    while (args[++i].lastIndexOf("'") != args[i].length() - 1) {
+                        if (i >= args.length - 1) {
+                            throw new CommandParseException2("string finished but the closing colon for inline body was not found");
+                        }
+                    }
+                }
+                case "-p" -> {
+                    if (map.get("-p") != null) {
+                        throw new CommandParseException2("Duplicate argument -p");
+                    }
+                    map.put("-p", true);
+                    i++;
+                }
+                default -> throw new CommandParseException2("There is an unknown argument: " + args[i]);
+            }
         }
     }
 }
