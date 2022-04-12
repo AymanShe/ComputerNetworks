119321a754d7c5a1bf299221d32af98e7c62592f
Ayman <aymalsh@gmail.com>
2022-04-11 23:56:39 -0400

    add new module for shared class Packet

---
+++ Shared/src/com/aymanshe/shared/PacketTypes.java
@@ -0,0 +1,9 @@
+package com.aymanshe.shared;
+
+public class PacketTypes {
+    public static final int SYN   = 0;
+    public static final int SYN_ACK = 1;
+    public static final int ACK = 2;
+    public static final int DATA  = 3;
+    public static final int FIN = 4;
+}
